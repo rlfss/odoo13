90789456bf420763f3a78e150cd4f437ebc0da22
Anviz <54052406+anviz-github@users.noreply.github.com>
2020-07-03 11:48:46 +0800

    Update sale.py
    update unmount

--- sale_discount_total/models/sale.py
+++ sale_discount_total/models/sale.py
@@ -35,15 +35,14 @@ class SaleOrder(models.Model):
         for order in self:
             amount_untaxed = amount_tax = amount_discount = 0.0
             for line in order.order_line:
-                #amount_untaxed += line.price_subtotal
-                amount_untaxed += line.price_unit*line.product_uom_qty
+                amount_untaxed += line.price_subtotal
                 amount_tax += line.price_tax
                 amount_discount += (line.product_uom_qty * line.price_unit * line.discount) / 100
             order.update({
                 'amount_untaxed': amount_untaxed,
                 'amount_tax': amount_tax,
                 'amount_discount': amount_discount,
-                'amount_total': amount_untaxed + amount_tax - amount_discount,
+                'amount_total': amount_untaxed + amount_tax,
             })
 
 
