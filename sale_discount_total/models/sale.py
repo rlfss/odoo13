09be2e0d70b82bfd62a2cd758b86a8c83bc8b631
Anviz <54052406+anviz-github@users.noreply.github.com>
2020-07-03 09:42:34 +0800

    Update sale.py
    digits=(16, 2) to digits=(16, 3)

--- sale_discount_total/models/sale.py
+++ sale_discount_total/models/sale.py
@@ -104,5 +104,5 @@ class SaleOrder(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    discount = fields.Float(string='Discount (%)', digits=(16, 2), default=0.0)
+    discount = fields.Float(string='Discount (%)', digits=(16, 3), default=0.0)
 
