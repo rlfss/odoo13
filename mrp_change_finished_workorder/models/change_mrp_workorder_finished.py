bc2eb1f32678661a1ed77e36ea3e394f4a612c8e
Anviz <54052406+anviz-github@users.noreply.github.com>
2020-09-24 20:18:29 +0800

    update

--- mrp_change_finished_workorder/models/change_mrp_workorder_finished.py
+++ mrp_change_finished_workorder/models/change_mrp_workorder_finished.py
@@ -6,7 +6,7 @@ from math import floor
 from odoo import api, fields, models, _, SUPERUSER_ID
 from odoo.exceptions import UserError
 from odoo.tools import float_compare, float_round
-class ChangeWorkorder(models.Model):
+class MrpWorkorder(models.Model):
     _description = 'Change Work Order done'
     _inherit = ['mrp.workorder']
 
@@ -16,4 +16,4 @@ class ChangeWorkorder(models.Model):
         if list(values.keys()) != ['time_ids'] and any(workorder.state == 'don' for workorder in self):
             raise UserError(_('You can not change the finished work order TEST.'))
 
-        return #super().write(values)
+        return super().write(values)
