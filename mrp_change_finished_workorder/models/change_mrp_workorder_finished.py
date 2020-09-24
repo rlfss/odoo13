029d1bf1f81848367893e03d7dabaea9b5932402
Anviz <54052406+anviz-github@users.noreply.github.com>
2020-09-25 00:12:13 +0800

    update

--- mrp_change_finished_workorder/models/change_mrp_workorder_finished.py
+++ mrp_change_finished_workorder/models/change_mrp_workorder_finished.py
@@ -12,30 +12,7 @@ class MrpWorkorder(models.Model):
 
     @api.model
     def write(self, values):
-        if 'production_id' in values:
-            raise UserError(_('You cannot link this work order to another manufacturing order.'))
-        if 'workcenter_id' in values:
-            for workorder in self:
-                if workorder.workcenter_id.id != values['workcenter_id']:
-                    if workorder.state in ('progress', 'done', 'cancel'):
-                        raise UserError(_('You cannot change the workcenter of a work order that is in progress or done.'))
-                    workorder.leave_id.resource_id = self.env['mrp.workcenter'].browse(values['workcenter_id']).resource_id
-        if list(values.keys()) != ['time_ids'] and any(workorder.state == 'done' for workorder in self):
-            raise UserError(_('You can not change the finished work order.'))
-        if 'date_planned_start' in values or 'date_planned_finished' in values:
-            for workorder in self:
-                start_date = fields.Datetime.to_datetime(values.get('date_planned_start')) or workorder.date_planned_start
-                end_date = fields.Datetime.to_datetime(values.get('date_planned_finished')) or workorder.date_planned_finished
-                if start_date and end_date and start_date > end_date:
-                    raise UserError(_('The planned end date of the work order cannot be prior to the planned start date, please correct this to save the work order.'))
-                # Update MO dates if the start date of the first WO or the
-                # finished date of the last WO is update.
-                if workorder == workorder.production_id.workorder_ids[0] and 'date_planned_start' in values:
-                    workorder.production_id.with_context(force_date=True).write({
-                        'date_planned_start': fields.Datetime.to_datetime(values['date_planned_start'])
-                    })
-                if workorder == workorder.production_id.workorder_ids[-1] and 'date_planned_finished' in values:
-                    workorder.production_id.with_context(force_date=True).write({
-                        'date_planned_finished': fields.Datetime.to_datetime(values['date_planned_finished'])
-                    })
-        return super(MrpWorkorder, self).write(values)
+        rec = super(MrpWorkorder, self).write(values)
+
+        print("rec",rec)
+        return rec
