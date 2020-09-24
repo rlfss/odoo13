f33e30dac5f896cc83ab4eb338a6055218f63a30
Anviz <54052406+anviz-github@users.noreply.github.com>
2020-09-24 18:49:32 +0800

    update

--- mrp_change_finished_workorder/models/change_mrp_workorder_finished.py
+++ mrp_change_finished_workorder/models/change_mrp_workorder_finished.py
@@ -15,5 +15,5 @@ class ChangeWorkorder(models.Model):
 
         if list(values.keys()) != ['time_ids'] and any(workorder.state == 'don' for workorder in self):
             raise UserError(_('You can not change the finished work order.'))
-        record = super().write(values)
-        return record
+
+        return super(ChangeWorkorder, self).write(values)
